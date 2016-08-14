fd1826d80bc65c4881e784c2f762ea2a7321c4c7
nikolaj schlüter.nielsen <nikolaj schlter.nielsen>
2016-08-14 17:21:27 +0200

    Trying new easing function.

--- sodaCanvas.js
+++ sodaCanvas.js
@@ -14,20 +14,13 @@ function rect(_x, _y, _width, _height, _color) {
 	};
 
 	this.animate = (parameter, newPosition, easing, time) => { //gør mere generical skal kunne modtage json amimaTIONER og sende dem aftsted til denne function 
-		
-		var totalFrames = time / 1000 * frameRate; // total frames acording to time set
-		var currentFrame = 0;
 
-		var oldPosition = this[parameter];
-		var distance = newPosition - oldPosition;
-			console.log({oldPosition}, {newPosition}, {distance})
-
-		var linearRatio = (totalFrames / easing(totalFrames));
-		var timeRatio = distance / totalFrames;
-			console.log({timeRatio, linearRatio})
+		var start = x;
+		var end = newPosition;
+		var total = time;
 
 		var clock = setInterval(() => {
-			this[parameter] = oldPosition + easing(currentFrame, totalFrames) * linearRatio * timeRatio
+			this[parameter] = linear(percent, elapsed, start, end, total)
 			currentFrame++;
 
 			if(currentFrame > totalFrames) clearInterval(clock);
@@ -48,55 +41,18 @@ function draw(){
 	sodaRect.draw();
 }
 
-var easing = function(t, totalFrames){
-
-	//return 1+(--t)*t*t*t*t
-	ret = t / Math.sqrt(1 + Math.pow(t, 2)) - 0.5
-	console.log(ret)
-	return ret
+var linear = function(percent,elapsed,start,end,total) {
+    return start+(end-start)*percent;
 }
+
+
 
 $( document ).ready(() => {
 	$( document ).click( () => {
 		var x = event.clientX;
 		var y = event.clientY;
-		sodaRect.animate('x', x, easing, 500)
-		/*.then(() => (sodaRect.animate('y', y, easing, 500)))
-		.then(() => (sodaRect.animate('x', (0), easing, 500)))
-		.then(() => (sodaRect.animate('y', (0), easing, 500)))*/
+		sodaRect.animate('x', x, easing, 1000)
 	})
 })
 
 
-
-
-
-
-EasingFunctions = {
-  // no easing, no acceleration
-  linear: function (t) { return t },
-  // accelerating from zero velocity
-  easeInQuad: function (t) { return t*t },
-  // decelerating to zero velocity
-  easeOutQuad: function (t) { return t*(2-t) },
-  // acceleration until halfway, then deceleration
-  easeInOutQuad: function (t) { return t<.5 ? 2*t*t : -1+(4-2*t)*t },
-  // accelerating from zero velocity 
-  easeInCubic: function (t) { return t*t*t },
-  // decelerating to zero velocity 
-  easeOutCubic: function (t) { return (--t)*t*t+1 },
-  // acceleration until halfway, then deceleration 
-  easeInOutCubic: function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 },
-  // accelerating from zero velocity 
-  easeInQuart: function (t) { return t*t*t*t },
-  // decelerating to zero velocity 
-  easeOutQuart: function (t) { return 1-(--t)*t*t*t },
-  // acceleration until halfway, then deceleration
-  easeInOutQuart: function (t) { return t<.5 ? 8*t*t*t*t : 1-8*(--t)*t*t*t },
-  // accelerating from zero velocity
-  easeInQuint: function (t) { return t*t*t*t*t },
-  // decelerating to zero velocity
-  easeOutQuint: function (t) { return 1+(--t)*t*t*t*t },
-  // acceleration until halfway, then deceleration 
-  easeInOutQuint: function (t) { return t<.5 ? 16*t*t*t*t*t : 1+16*(--t)*t*t*t*t }
-}
