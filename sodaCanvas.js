54c281562edc9b719bcb60e7545ccbd60617c4f2
nikolaj schlüter.nielsen <nikolaj schlter.nielsen>
2016-08-19 01:13:15 +0200

    fixes #10

--- sodaCanvas.js
+++ sodaCanvas.js
@@ -4,27 +4,40 @@ var frameRate = 60;
 
 
 function rect(_x, _y, _width, _height, _color) {
-	this.emptyPromise = (parameter) => {
-		return new Promise((resolve, reject) => resolve()) 
-	};
+	this.emptyPromise = (parameter) => new Promise((resolve, reject) => resolve()) 
+	
 	this.currentlyAnimating = new Array();
 	this.breakAnimation = new Array();
+
 
 	this.x = _x;
 	this.y = _y;
 	this.width = _width;
 	this.height = _height;
-	this.color = _color;
+
+	this.color = w3color(_color, null)
+
+	this.rColor = this.color.red;
+	this.gColor = this.color.green;
+	this.bColor = this.color.blue;
+	this.aColor = this.color.opacity;
+
+	this.rbg = function () {
+		return "rgba("+Math.round(this.rColor)+","+Math.round(this.gColor)+","+Math.round(this.bColor)+","+ Math.round(this.aColor) +")";
+	}
+
 	this.center = [this.width/2, this.height/2]; // not opdated when w / h is changed.. Should be done via get/set
 	
+	//this.animateColor ?= 
 
 	this.draw = () => {
-		ctx.fillStyle = this.color;
+		ctx.fillStyle = this.rbg(); //could be optimized
 		ctx.fillRect(this.x, this.y, this.width, this.height);
 	};
 
 	this.break = sodaCanvas.break
 	this.animateSingle = sodaCanvas.animateSingle;
 	this.animate = sodaCanvas.animate;
+	
 };
 
