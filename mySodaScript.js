9c6835d95c074f36542d3b8e761bb84834d7acd4
nikolaj schlüter.nielsen <nikolaj schlter.nielsen>
2016-08-21 16:01:37 +0200

    Cool new animation example

--- mySodaScript.js
+++ mySodaScript.js
@@ -1,36 +1,58 @@
 
 setInterval(() => draw(), (1000/frameRate))
 
-var bg = new rect(0, 0, 1000, 700, "#EEEEEE");
+var bg = new rect(0, 0, 1920, 1000, "white");
 bg.center = [0,0]
-var sodaRect = new rect(100, 100, 30, 30, "grey");
+var sodaRect = new image(1920/2 - 276/2, 500-276/2 - 5, 200, 200, "logosmall.png");
+var sodaRects = new Array()
+
+for (var i = 0; i < 200; i++) {
+	sodaRects[i] = new rect(20*i, 0, 100, 1000, "white")
+}
 
 function draw(event){
 	bg.draw();
+
+	for (var i = 0; i < 200; i++) {
+		sodaRects[i].draw();
+	}
 	sodaRect.draw();
+
 }
 
-$( document ).ready(() => {
-	$( document ).click( () => {
-		
-		sodaRect.animate({
-			rColor: Math.random()*256,
-			bColor: Math.random()*256,
-			gColor: Math.random()*256,
-		}, easing.easeOutExpo, 2000)
-
-		var x = event.clientX;
-		var y = event.clientY;
-
-		sodaRect.animate({
-			width:100,
-			height:100,
-			x: x,
-			y: y,
-		}, easing.easeInExpo, 1000)
-		.then(() => sodaRect.animate({
-			width:50,
-			height:50,
-		}, easing.easeInQuad, 500) )
-	})//.then(()=>loop())
+//$( document ).ready(() => {
+//sodaRect.clicked((mouseEvent) => console.log(mouseEvent))
+$( document ).click( () => {
+	sodaRect.animate({aColor: 0}, easing.easeInOutExpo, 1000).then(() => sodaRect.animate({aColor:0.6}, easing.easeInOutExpo, 1000))
 })
+
+loop();
+function loop(){
+
+	for (var i = 0; i < 200; i++) {
+		sodaRects[i].animate({
+			width:10,
+		}, easing.easeInOutExpo, 1000)
+	}
+	setTimeout(() => {
+		c.style.cursor = 'pointer'
+	for (var i = 0; i < 200; i++) {
+		sodaRects[i].animate({
+			rColor: Math.random() * 256*2 + 100,
+			bColor: Math.random() * 0 + 100,
+			gColor: Math.random() * 256/3 + 100,
+			height: Math.random() * 500 + 250,
+		}, easing.easeInOutExpo, 1000)
+	}
+	}, 1000);
+
+	setTimeout(() => {
+		c.style.cursor = ''
+		for (var i = 0; i < 200; i++) {
+			sodaRects[i].animate({
+				width:0
+			}, easing.easeInOutExpo, 1000)
+		}
+	}, 2000)
+	setTimeout(() => loop(), 3000);
+}
--- sodaCanvas.js
+++ sodaCanvas.js
@@ -6,8 +6,15 @@ var onClick = document.createAttribute("onclick")
 onClick.value = 'mouseClick(event)'
 c.setAttributeNode(onClick);
 
+var mouseClicked = false;
+
 function mouseClick(event){
 	console.log(event)
+	mouseClicked = true;
+}
+
+function mouseRelease(event){
+	mouseClicked = false;
 }
 
 
@@ -23,7 +30,7 @@ function rect(_x, _y, _width, _height, _color) {
 	this.width = _width;
 	this.height = _height;
 
-	this.color = w3color(_color, null)
+	this.color = w3color(_color, null);
 
 	this.rColor = this.color.red;
 	this.gColor = this.color.green;
@@ -31,14 +38,24 @@ function rect(_x, _y, _width, _height, _color) {
 	this.aColor = this.color.opacity;
 
 	this.rbg = function () {
-		return "rgba("+Math.round(this.rColor)+","+Math.round(this.gColor)+","+Math.round(this.bColor)+","+ Math.round(this.aColor) +")";
+		return "rgba("+Math.round(this.rColor)+","+Math.round(this.gColor)+","+Math.round(this.bColor)+","+ (this.aColor) +")";
 	}
 
 	this.center = [this.width/2, this.height/2]; // not opdated when w / h is changed.. Should be done via get/set
-	
-	//this.animateColor ?= 
+
+	this.clicked = (inpFunction) => {
+		return new Promise((resolve, reject) => {
+			if(mouseClicked){
+				inpFunction();
+				resolve();
+			}
+		})
+
+	}	
+
 
 	this.draw = () => {
+
 		ctx.fillStyle = this.rbg(); //could be optimized
 		ctx.fillRect(this.x, this.y, this.width, this.height);
 	};
@@ -52,3 +69,69 @@ function rect(_x, _y, _width, _height, _color) {
 	
 };
 
+function circle(_x, _y, _radius, _color) {
+	this.emptyPromise = (parameter) => new Promise((resolve, reject) => resolve()) 
+	
+	this.currentlyAnimating = new Array();
+	this.breakAnimation = new Array();
+
+
+	this.x = _x;
+	this.y = _y;
+	this.radius = _radius;
+
+	this.color = w3color(_color, null);
+
+	this.rColor = this.color.red;
+	this.gColor = this.color.green;
+	this.bColor = this.color.blue;
+	this.aColor = this.color.opacity;
+
+	this.rbg = function () {
+		return "rgba("+Math.round(this.rColor)+","+Math.round(this.gColor)+","+Math.round(this.bColor)+","+ (this.aColor) +")";
+	}
+
+	this.draw = () => {
+		ctx.fillStyle = this.rbg(); //could be optimized
+		ctx.arc(this.x, this.y, this.radius, 0, 2*Math.PI);
+		ctx.fill();
+	};
+
+	
+	
+	this.break = sodaCanvas.break
+	this.animateSingle = sodaCanvas.animateSingle;
+	this.animate = sodaCanvas.animate;
+	//this.clicked = ()
+	
+};
+
+function image(_x, _y, _width, _height, imageLink) {
+	this.emptyPromise = (parameter) => new Promise((resolve, reject) => resolve()) 	
+	this.currentlyAnimating = new Array();
+	this.breakAnimation = new Array();
+	this.x = _x;
+	this.y = _y;
+	this.width = _width;
+	this.height = _height;
+	this.center = [this.width/2, this.height/2]; // not opdated when w / h is changed.. Should be done via get/set
+	this.rotation; //unimplemented. //ctx.rotate(0.3) 
+	this.imageFile = new Image();
+	this.imageFile.src = imageLink;
+
+	this.draw = () => {
+		if(this.width === 0 && this.height === 0){ // if this were to be animated down to 0. it would draw differently.
+			var widthToApply = this.width ? this.width : null;
+			var heightToApply = this.height ? this.height : null; 
+			ctx.drawImage(this.imageFile, this.x, this.y, widthToApply, heightToApply);
+		}else{
+			ctx.drawImage(this.imageFile, this.x, this.y);
+		}
+		
+	};
+
+
+	this.break = sodaCanvas.break
+	this.animateSingle = sodaCanvas.animateSingle;
+	this.animate = sodaCanvas.animate;
+}
