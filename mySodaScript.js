ee204eb4210072ab559ee45cac4e3975c26e3569
Nikolaj Schlüter <nikolaj@bitkompagniet.dk>
2016-08-25 01:52:32 +0200

    Add stuff. Ready to fucking merge

--- mySodaScript.js
+++ mySodaScript.js
@@ -6,19 +6,20 @@ bg.center = [0,0]
 var sodaRect = new image(1920/2 - 276/2, 500-276/2 - 5, 200, 200, "logosmall.png");
 var sodaRects = new Array()
 
-for (var i = 0; i < 200; i++) {
+for (var i = 0; i < 100; i++) {
 	sodaRects[i] = new rect(20*i, 0, 100, 1000, "white")
 }
 
 function draw(event){
 	bg.draw();
 
-	for (var i = 0; i < 200; i++) {
+	for (var i = 0; i < 100; i++) {
 		sodaRects[i].draw();
 	}
 	sodaRect.draw();
-
+	
 }
+
 
 //$( document ).ready(() => {
 //sodaRect.clicked((mouseEvent) => console.log(mouseEvent))
@@ -29,14 +30,16 @@ $( document ).click( () => {
 loop();
 function loop(){
 
-	for (var i = 0; i < 200; i++) {
+	for (var i = 0; i < 100; i++) {
 		sodaRects[i].animate({
-			width:10,
+			width:2,
 		}, easing.easeInOutExpo, 1000)
 	}
-	setTimeout(() => {
-		c.style.cursor = 'pointer'
-	for (var i = 0; i < 200; i++) {
+
+	setTimeout(function(){console.log('hej')}, 1000)
+
+	setTimeout(function() {
+	for (var i = 0; i < 100; i++) {
 		sodaRects[i].animate({
 			rColor: Math.random() * 256*2 + 100,
 			bColor: Math.random() * 0 + 100,
@@ -48,7 +51,7 @@ function loop(){
 
 	setTimeout(() => {
 		c.style.cursor = ''
-		for (var i = 0; i < 200; i++) {
+		for (var i = 0; i < 100; i++) {
 			sodaRects[i].animate({
 				width:0
 			}, easing.easeInOutExpo, 1000)
